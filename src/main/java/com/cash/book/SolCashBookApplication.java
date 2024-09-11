efc793048012b0a9ee10e05ef317088e5d227f2a
soraHan <guswnd031@hanmaill.net>
2024-09-11 12:24:23 +0900

    최초커밋

---
+++ src/main/java/com/cash/book/SolCashBookApplication.java
@@ -0,0 +1,13 @@
+package com.cash.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolCashBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolCashBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cash/book/SolCashBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cash.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolCashBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
